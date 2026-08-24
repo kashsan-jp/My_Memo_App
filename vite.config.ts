f9f002d53780566f13f5fcf617c3d5f4c565922e
Kashsan <kashsan_jp@yahoo.co.jp>
2026-08-24 16:35:37 +0900

    2026_8_24_5

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,32 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ 
-      registerType: 'autoUpdate',
-      // includeAssets: ['assets/favicon.ico'],
+    VitePWA({
+      devOptions: { enabled: true },
       manifest: {
-      name: 'ぷちメモ',
-      short_name: 'petit_memo',
-      description: 'k-ash app',
-      theme_color: '#ffffff',
-      icons: [
-        {
-          src: 'assets/192_petit_memo.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'assets/512_petit_memo.png',
-          sizes: '512x512',
-          type: 'image/png'
-       }
-      ]
-    }
-  })
-  ]
+        name: "ぷちメモ",
+        icons: [
+          {
+            sizes: "192x192",
+            src: "192_petit_memo.png",
+            type: "image/png",
+          },    
+          {
+            sizes: "512x512",
+            src: "512_petit_memo.png",
+            type: "image/png",
+          },
+          {
+            sizes: "512x512",
+            src: "512_petit_memo.png",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
