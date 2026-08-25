d9a0b2a40e5a8aead7349e0f647560a5f40cae58
Kashsan <kashsan_jp@yahoo.co.jp>
2026-08-25 18:56:02 +0900

    2026_8_25

--- vite.config.ts
+++ vite.config.ts
@@ -6,28 +6,45 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      devOptions: { enabled: true },
+      // devOptions: { enabled: true },
+      // manifest: {
+      //   name: "ぷちメモ",
+      //   icons: [
+      //     {
+      //       sizes: "192x192",
+      //       src: "192_petit_memo.png",
+      //       type: "image/png",
+      //     },    
+      //     {
+      //       sizes: "512x512",
+      //       src: "512_petit_memo.png",
+      //       type: "image/png",
+      //     },
+      //     {
+      //       sizes: "512x512",
+      //       src: "512_petit_memo.png",
+      //       type: "image/png",
+      //       purpose: "maskable",
+      //     },
+      //   ],
+      // },
+
+      registerType: 'autoUpdate',
+      // public ディレクトリを基準とした相対パスで指定します
+      includeAssets: ['assets/favicon.png', 'robots.txt', 'apple-touch-icon.png'],
       manifest: {
-        name: "ぷちメモ",
+        name: 'My React PWA',
+        short_name: 'ReactPWA',
+        description: 'React Vite PWA Application',
+        theme_color: '#ffffff',
         icons: [
           {
-            sizes: "192x192",
-            src: "192_petit_memo.png",
-            type: "image/png",
-          },    
-          {
-            sizes: "512x512",
-            src: "512_petit_memo.png",
-            type: "image/png",
-          },
-          {
-            sizes: "512x512",
-            src: "512_petit_memo.png",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
+            src: 'assets/favicon.ico', // ここも同様のパスになります
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon'
+          }
+        ]
+      }
     }),
   ],
 });
